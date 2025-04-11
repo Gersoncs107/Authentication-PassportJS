aaac78db453833721fc974706d9629b25beda11d
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-10 22:03:06 -0300

    authentication

--- app.js
+++ app.js
@@ -87,12 +87,13 @@ passport.use(
     new LocalStrategy( async (username, password, done) => {
         try{
             const {rows} = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+            const match = await bcrypt.compare(password, user.password);
             const user = rows[0];
             if(!user){
                 done(null, false, {message: "Incorrect username"})
             }
-            if(user.password !== password){
-                done(null, false, {message: "Incorrect password"})
+            if(!match){
+              return done(null, false, {message: "Incorrect password"})
             }
             return done(null, user)
         } catch(err){
